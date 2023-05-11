ac0061f1f985fee43605ac19edce325a14b6edf9
Ragnorosss <gost45678@gmail.com>
2023-05-11 22:45:37 +0300

    Other

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
-
+import Header from "section/Header";
+import Hero from "section/Hero";
+import Footer from "section/Footer";
+import "style/reset.scss"
 function App() {
   return (
     <div className="App">
+      <Header/>
+      <Hero/>
       <h1>Hello world</h1>
+      <Footer/>
     </div>
   );
 }
