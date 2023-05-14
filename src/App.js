7b5e7277f8799e02a6c78c025ad4821e401189cb
Ragnorosss <gost45678@gmail.com>
2023-05-14 20:09:16 +0300

    add pages about

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
-import Header from "section/Header";
-import Hero from "section/Hero";
-import Footer from "section/Footer";
+import About from "pages/About";
 import "style/_styles.scss"
 
 function App() {
+  
   return (
     <div className="App">
-      <Header/>
-      <Hero/>
-      <h1>Hello world</h1>
-      <Footer/>
+      <About/>
     </div>
   );
 }
