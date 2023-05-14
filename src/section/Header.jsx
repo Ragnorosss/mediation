2d7a9dcc312cdc822b79afe58d2f1666f20543f9
Ragnorosss <gost45678@gmail.com>
2023-05-14 16:43:20 +0300

    Add Btn module in header

--- src/section/Header.jsx
+++ src/section/Header.jsx
@@ -1,7 +1,34 @@
-import React from 'react'
-
+import Button from "UI/button/Button.module"
 export default function Header() {
   return (
-    <div>Header</div>
+    <header className='header'>
+      <div className="header__container">
+        <div className="header__wrapper">
+          <a href="#" className="header__logo"></a>
+          <nav className="header__menu">
+            <ul className="menu__list">
+              <li className="list__item">
+                <a  className='list__item-link' href="#">Головна</a>
+              </li>
+              <li className="list__item">
+                <a className='list__item-link' href="#">Спогади</a>
+              </li>
+              <li className="list__item">
+                <a className='list__item-link' href="#">Бібліотека</a>
+              </li>
+              <li className="list__item">
+                <a className='list__item-link' href="#">Контакти</a>
+              </li>
+            </ul>
+            <div className="reg">
+              <Button>Стати автором</Button>
+              <Button>Увійти</Button>
+            </div>
+          </nav>
+          <Button className='changes__theme'>3</Button>
+          <Button className='changes__theme'>4</Button>
+        </div>
+      </div>
+    </header>
   )
 }
