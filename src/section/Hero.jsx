cced98b066b6134b627e3880d47838e2b9184afe
Ragnorosss <gost45678@gmail.com>
2023-05-15 14:09:38 +0300

    Add struc

--- src/section/Hero.jsx
+++ src/section/Hero.jsx
@@ -2,6 +2,16 @@ import React from 'react'
 
 export default function Hero() {
   return (
-    <div>Hero</div>
+    <section className='hero'>
+      <div className="hero__container">
+        <div className="hero__wrapper">
+          <div className="hero__info">
+            <h1>We help you find the best solution </h1>
+            <p>Історія медиації в Україні - некоммерційний проект, що збирає інформацію про Медиацію в Україні Et has minim elitr intellegat. Вже зібрано близько 10 000 статей. Mea aeterno eleifend antiopam ad, nam no suscipit quaerendum. At nam minimum ponderum. Est audiam animal molestiae te. Ex duo eripuit mentitum.</p>
+            <div className="donate"></div>
+          </div>
+        </div>
+      </div>
+    </section>
   )
 }
