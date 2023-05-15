0366d7464e0d2feb6924829106621cd70b88ae42
Ragnorosss <gost45678@gmail.com>
2023-05-15 15:43:21 +0300

    finished with section hero

--- src/section/Hero.jsx
+++ src/section/Hero.jsx
@@ -1,4 +1,5 @@
-import React from 'react'
+import Button from "UI/button/Button.module";
+import bac_img from "assets/background_hero.png";
 
 export default function Hero() {
   return (
@@ -6,10 +7,14 @@ export default function Hero() {
       <div className="hero__container">
         <div className="hero__wrapper">
           <div className="hero__info">
-            <h1>We help you find the best solution </h1>
-            <p>Історія медиації в Україні - некоммерційний проект, що збирає інформацію про Медиацію в Україні Et has minim elitr intellegat. Вже зібрано близько 10 000 статей. Mea aeterno eleifend antiopam ad, nam no suscipit quaerendum. At nam minimum ponderum. Est audiam animal molestiae te. Ex duo eripuit mentitum.</p>
-            <div className="donate"></div>
+            <h1 className="hero__info-title">We help you find the best solution </h1>
+            <p className="hero__info-subtitle">Історія медиації в Україні - некоммерційний проект, що збирає інформацію про Медиацію в Україні Et has minim elitr intellegat. Вже зібрано близько 10 000 статей. Mea aeterno eleifend antiopam ad, nam no suscipit quaerendum. At nam minimum ponderum. Est audiam animal molestiae te. Ex duo eripuit mentitum.</p>
+            <div className="donate">
+              <Button><a href="/">Підтримати</a></Button>
+              <Button><a href="/">Написати нам</a></Button>
+            </div>
           </div>
+          <img src={bac_img} alt="hero-img"/>
         </div>
       </div>
     </section>
