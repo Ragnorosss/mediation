bf78ebedf2af6af34d9f9ef0fd4a11c8f776eb30
Vavilov Nikita <67798734+Overlord228-png@users.noreply.github.com>
2023-05-15 21:00:05 +0300

    Add Main Page

---
+++ src/pages/Main.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Header from 'section/Header'
+import MainInfo from 'section/MainInfo'
+
+export default function Catalog() {
+    return (
+        <>
+            <Header/>
+            <MainInfo />
+        </>
+    )
+}
