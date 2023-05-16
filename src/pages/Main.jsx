9d7bb576f84c4384b157f98980b6d81f3e1fe011
Vavilov Nikita <67798734+Overlord228-png@users.noreply.github.com>
2023-05-16 23:00:32 +0300

    add import page Maininfo

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import Header from 'section/Header'
-import MainInfo from 'section/MainInfo'
+import MainInfo from 'section/Maininfo'
 
 export default function Catalog() {
     return (
         <>
             <Header/>
-            <MainInfo />
+            <MainInfo/>
         </>
     )
 }
