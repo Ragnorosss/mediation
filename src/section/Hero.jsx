8fb96908921078ab4440250b08f45ec368cad18c
Ragnorosss <gost45678@gmail.com>
2023-05-16 22:36:59 +0300

    remove Button

--- src/section/Hero.jsx
+++ src/section/Hero.jsx
@@ -1,4 +1,3 @@
-import Button from "UI/button/Button.module";
 import bac_img from "assets/background_hero.png";
 
 export default function Hero() {
@@ -10,8 +9,8 @@ export default function Hero() {
             <h1 className="hero__info-title">We help you find the best solution </h1>
             <p className="hero__info-subtitle">Історія медиації в Україні - некоммерційний проект, що збирає інформацію про Медиацію в Україні Et has minim elitr intellegat. Вже зібрано близько 10 000 статей. Mea aeterno eleifend antiopam ad, nam no suscipit quaerendum. At nam minimum ponderum. Est audiam animal molestiae te. Ex duo eripuit mentitum.</p>
             <div className="donate">
-              <Button><a href="/">Підтримати</a></Button>
-              <Button><a href="/">Написати нам</a></Button>
+              <a href="/">Підтримати</a>
+              <a href="/">Написати нам</a>
             </div>
           </div>
           <img src={bac_img} alt="hero-img"/>
