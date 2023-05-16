d567b361de50a6d65c9ff6e6a064be63de8ebe04
Vavilov Nikita <67798734+Overlord228-png@users.noreply.github.com>
2023-05-16 23:01:53 +0300

    created className and add block video

--- src/section/MainInfo.jsx
+++ src/section/MainInfo.jsx
@@ -3,35 +3,48 @@ import Avatar from "../assets/avatar.svg"
 
 export default function MainInfo() {
     return (
-        <div className='informational-user'>
-            <div className='information-basic'>
-                <img className="logo-img" src={Avatar}/>
-                <div>
-                    <h1 className='text-title'>Name Surname</h1>
-                    <p className='text-name-city city-user'>Одесса</p>
+        <section className="main">
+            <div className="wrapper">
+                <div className="main__wrapper">
+                    <img className="logo-avatar" src={Avatar}/>
+                    <div className=''>
+                        <h1 className='title-text'>Name Surname</h1>
+                        <p className='city-user title-subtitle'>Одесса</p>
+                    </div>
+                    <div className=''>
+                        <h1 className='title-text'>Квалiфiкацiя</h1>
+                        <p className='title-subtitle-profesion'>Профеciйний медiатор</p>
+                    </div>
+                    <button className='btn-text btn-create'>Змінити особисту інформацію</button>
                 </div>
-                <div>
-                    <h1 className='text-title'>Квалiфiкацiя</h1>
-                    <p>Профеciйний медiатор</p>
+                <div className='biogrfi__wrapper'>
+                    <h1 className='title-text-biogrfi'>Біографiя</h1>
+                    <p className='text-about-user text-desk'>
+                        Мене звати Iван, я професийний медiатор.
+                        Мене звати Iван, я професийний медiатор.
+                        Мене звати Iван, я професийний медiатор.
+                        Мене звати Iван, я професийний медiатор.
+                        Мене звати Iван, я професийний медiатор.
+                        Мене звати Iван, я професийний медiатор.
+                        Мене звати Iван, я професийний медiатор.
+                        Мене звати Iван, я професийний медiатор.
+                        Мене звати Iван, я професийний медiатор.
+                        Мене звати Iван, я професийний медiатор.
+                        Мене звати Iван, я професийний медiатор.
+                    </p>
                 </div>
-                <button className='btn-text btn-create'>Змінити особисту інформацію</button>
             </div>
-            <div className='info-biogrfi'>
-                <h1 className='text-title'>Біографiя</h1>
-                <p className='text about-user'>
-                    Мене звати Iван, я професийний медiатор.
-                    Мене звати Iван, я професийний медiатор.
-                    Мене звати Iван, я професийний медiатор.
-                    Мене звати Iван, я професийний медiатор.
-                    Мене звати Iван, я професийний медiатор.
-                    Мене звати Iван, я професийний медiатор.
-                    Мене звати Iван, я професийний медiатор.
-                    Мене звати Iван, я професийний медiатор.
-                    Мене звати Iван, я професийний медiатор.
-                    Мене звати Iван, я професийний медiатор.
-                    Мене звати Iван, я професийний медiатор.
-                </p>
+            {/* Videos start */}
+            <div className="block-video">
+                <div className="video-wrapper">
+                    <div className="video-item"></div>
+                    <iframe className='video__element' src="https://www.youtube.com/embed/watch?v=oXW4cnKCzsM" 
+                    frameborder="1" 
+                    allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" 
+                    allowfullscreen></iframe>
+                </div>
             </div>
-        </div>
+            {/* Videos end */}
+        </section>
     )
 }
