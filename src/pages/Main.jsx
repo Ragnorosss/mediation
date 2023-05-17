523e595184a369dc164013a61c618596f82dc603
Vavilov Nikita <67798734+Overlord228-png@users.noreply.github.com>
2023-05-17 19:15:13 +0300

    add page Main.jsx, and added to it 2 components import Header and Maininfo

---
+++ src/pages/Main.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Header from 'section/Header'
+import MainInfo from 'section/Maininfo'
+
+export default function Main() {
+	return (
+    	<>
+        	<Header />
+        	<MainInfo/>
+    	</>
+  )
+}
