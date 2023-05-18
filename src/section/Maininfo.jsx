5ec37f5491b746aa95c7188e29c470e5267b5565
Vavilov Nikita <67798734+Overlord228-png@users.noreply.github.com>
2023-05-18 21:11:08 +0300

    added block мои статьї, сделал две карточки для статей

--- src/section/Maininfo.jsx
+++ src/section/Maininfo.jsx
@@ -54,86 +54,85 @@ export default function MainInfo() {
             <br/>
 
             {/* mine article */}
-            <div className="">
-                <h1 className='title-text-article'>Мої статтi:</h1>
-                <div className="search-input">
-                    <input type="" placeholder='Введiть назву статтi для пошуку'/>
-                </div>
-                <div className="card-urticle-new">
-                    <div className="">
-                        <img src={Avatar} alt="" />
-                        <h3>Name</h3>
-                        <p>сьогодні о 15:23</p>
+            <div className="main-article">
+                <div className="article-wrapper">
+                    <p className='text-article'>Мої статтi:</p>
+                    <input className="search-input text-input" placeholder='Введiть назву статтi для пошуку'/>
+                    <div className="card-urticle-new">
+                        <div className="section-info">
+                            <img className='avatar' src={Avatar} alt="" />
+                            <h3 className='text-name'>Name</h3>
+                            <p className='data-post'>сьогодні о 15:23</p>
+                        </div>
+                        <div className="title-article">
+                            <p className='text-name-article'>Назва статті</p>
+                            <div className="item-subject">
+                                <ul className='ul-list'>
+                                    <li className='list-item'>1</li>
+                                    <li className='list-item'>2</li>
+                                    <li className='list-item'>3</li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div className="articl-block-info">
+                            <p className='text-articl-info'>
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                                Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                                Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+                            </p>
+                        </div>
+                        <div className="btn-block">
+                            <button className='btn-download text-btn-download'>Завантажити</button>
+                            <button className='btn-create text-btn-create'>Змінити</button>
+                        </div>
                     </div>
-                    <div className="title-article">
-                        <p>Назва статті</p>
-                        <div className="item-subject">
-                            <nav>
+
+                    <br/>
+
+                    <div className="card-urticle-editinig">
+                        <div className="section-info-2">
+                            <img className='avatar' src={Avatar} alt="" />
+                            <h3 className='text-name'>Name</h3>
+                            <p className='text-article-creat'>наразі стаття редагується</p>
+                        </div>
+                        <div className="title-article-aside">
+                            <p>Назва статті</p>
+                            <div className="item-subject">
+                                <p>додати тему</p>
+                            </div>
+                        </div>
+                        <div className="articl-block-info">
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                            Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+                        </div>
+                        <div className="btn-block-created">
+                            <div className="btn-block-start">
+                                <button className='text-btn-attach btn-attach'>Прикріпити файли</button>
+                                <button className='text-btn-saved btn-saved'>Зберегти</button>
+                            </div>
+                            <button className='text-btn-deleted btn-deleted'>Видалити статтю</button>
+                        </div>
+                    </div>
+
+                    <br/>
+                    <br/>
+                    <br/>
+                    <br/>
+
+                    <div className="paginator">
+                        <div className="">
+                            <p>Назад</p>
+                            <div className="">
                                 <ul>
                                     <li>1</li>
                                     <li>2</li>
                                     <li>3</li>
+                                    <li>4</li>
                                 </ul>
-                            </nav>
+                            </div>
+                            <p>Вперед</p>
                         </div>
-                    </div>
-                    <div className="articl-block-info">
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                        Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-                    </div>
-                    <div className="btn-block">
-                        <button>Завантажити</button>
-                        <button>Змінити</button>
-                    </div>
-                </div>
-
-                <br/>
-                <br/>
-                <br/>
-                <br/>
-
-                <div className="card-urticle-editinig">
-                    <div className="">
-                        <img src={Avatar} alt="" />
-                        <h3>Name</h3>
-                        <p>наразі стаття редагується</p>
-                    </div>
-                    <div className="title-article">
-                        <p>Назва статті</p>
-                        <div className="item-subject">
-                            <p>додати тему</p>
-                        </div>
-                    </div>
-                    <div className="articl-block-info">
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                        Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-                    </div>
-                    <div className="btn-block">
-                        <button>Завантажити</button>
-                        <button>Змінити</button>
-                        <button>Видалити статтю</button>
-                    </div>
-                </div>
-
-                <br/>
-                <br/>
-                <br/>
-                <br/>
-
-                <div className="paginator">
-                    <div className="">
-                        <p>Назад</p>
-                        <div className="">
-                            <ul>
-                                <li>1</li>
-                                <li>2</li>
-                                <li>3</li>
-                                <li>4</li>
-                            </ul>
-                        </div>
-                        <p>Вперед</p>
                     </div>
                 </div>
             </div>
